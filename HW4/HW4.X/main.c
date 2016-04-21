03aa5d76f4daf18444d46d1e0526eea6b411dc27
HailiangHu <hailianghu2015@u.northwestern.edu>
2016-04-20 20:57:09 -0500

    spi output square wave

--- HW4/HW4.X/main.c
+++ HW4/HW4.X/main.c
@@ -1,6 +1,5 @@
-//#include "NU32.h"       // constants, funcs for startup and UART
-// Demonstrates spi by accessing external ram
-// PIC is the master, ram is the slave
+// Demonstrates spi by accessing external dac
+// PIC is the master, dac is the slave
 // Uses microchip 23K256 ram chip (see the data sheet for protocol details)
 // SDO4 -> SI (pin F5 -> pin 5)
 // SDI4 -> SO (pin F4 -> pin 2)
@@ -17,8 +16,10 @@
 
 #include<xc.h>           // processor SFR definitions
 #include<sys/attribs.h>  // __ISR macro
+#include <math.h>
 #define SYS_FREQ 48000000 // system frequency 48MHz
-#define CS LATBbits.LATB7       // chip select pin
+#define CS LATBbits.LATB7       // chip select pin latb7
+#define PI 3.1415926
 // DEVCFG0
 #pragma config DEBUG = OFF // no debugging
 #pragma config JTAGEN = OFF // no jtag
@@ -63,28 +64,27 @@ unsigned char spi_io(unsigned char o) {
   return SPI1BUF;
 }
 
-// initialize spi4 and the ram module
-void dac_init() {
+// initialize spi1 
+void initSPI1() {
   // set up the chip select pin as an output
-  // the chip select pin is used by the sram to indicate
   // when a command is beginning (clear CS to low) and when it
   // is ending (set CS high)
-  TRISBbits.TRISB7 = 0; // select pin as output 	
+  TRISBbits.TRISB7 = 0; // select b7 pin as output 	
   CS = 1;// output high
 
-  // Master - SPI4, pins are: SDI4(F4), SDO4(F5), SCK4(F13).  
+  // Master - SPI1, pins are: SDI1(F4), SDO1(F5), SCK1(F13).  
   // we manually control SS4 as a digital output (F12)
   // since the pic is just starting, we know that spi is off. We rely on defaults here
  
-  // setup spi4
-  RPB8Rbits.RPB8R = 0b0011;
+  // setup spi1
+  RPB8Rbits.RPB8R = 0b0011;	// set RPB8 as SDOut
   SPI1CON = 0;              // turn off the spi module and reset it
   SPI1BUF;                  // clear the rx buffer by reading from it
   SPI1BRG = 0x3;            // baud rate to 10 MHz [SPI4BRG = (80000000/(2*desired))-1]
   SPI1STATbits.SPIROV = 0;  // clear the overflow bit
   SPI1CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
   SPI1CONbits.MSTEN = 1;    // master operation
-  SPI1CONbits.ON = 1;       // turn on spi 4
+  SPI1CONbits.ON = 1;       // turn on spi 1
   
 
                             // send a ram set status command.
@@ -94,26 +94,88 @@ void dac_init() {
 //  CS = 1;                   // finish the command
 }
 
+void setVoltage(char channel, unsigned int voltage) {
+		
+//	int dig_vol = 0;
+//	int data_sent = 0;
+//	dig_vol = (2^8 * floor(voltage / 3.3));
+	switch(channel) {
 
+	case 'A' :
+   		//data_sent = 0x7000 | (dig_vol<<4);
+		//data_sent = 0x70 | (voltage>>4);
+        CS = 0;
+        spi_io(0x70 | (voltage>>4));
+        spi_io(voltage<<4);
+        CS = 1;
+    break; 
+	
+	case 'B' :
+   		//data_sent = 0xF000 | (dig_vol<<4);
+    	//data_sent = 0xF000 | (voltage<<4);
+        CS = 0;
+        spi_io(0xF0 | (voltage>>4));
+        spi_io(voltage<<4);
+        CS = 1;
+    break; 
+
+    }
+//	CS = 0;
+//	spi_io(data_sent & 0xFF00>> 8);  // the most significant byte
+//	spi_io(data_sent & 0x00FF);      // the least significant byte    
+//	CS = 1;  
+}
 
 
 
 int main(void) {
-  
-  
+  char channel = 'A';
+  int i = 1;
+  float output = 0;
+  float t = 0;
+  unsigned int output_dig = 0;
+	  
   
   TRISAbits.TRISA4 = 0;       // make A4(PORT 12) as output
  // NU32_Startup();   // cache on, interrupts on, LED/button init, UART init
-  dac_init(); 
+  initSPI1(); 
   LATAbits.LATA4 = 1;
   // check the ram status
-  CS = 0;
   
-  spi_io(0x78);                                      // ram read status command
-  spi_io(0x00);                               // the actual status
+  CS = 0;  
+  spi_io(0x70);                                   
+  spi_io(0x00);      //write 16 bits into dac                      
   CS = 1;
 
+  channel = 'A';
+ 
+  setVoltage('A', 0x80);
+  _CP0_SET_COUNT(0);//initialize the counter, half the speed of CPU
   while(1) {
+      
+      if( _CP0_GET_COUNT() >= (SYS_FREQ/1000/2)*1 )// count up to 1ms // greater than (SYS_FREQ /2*countingseconds)
+            {
+                output = (2^8) * (sin(2*PI*1*t) +0.5);//sin(2*pi*f*t)
+                //output_scale = 2^8 * output ;
+                output_dig = (unsigned int) output;
+                output_dig = output_dig & 0xFF;
+                //setVoltage('A', 0x80);
+                setVoltage('A', output_dig);
+                _CP0_SET_COUNT(0); // reset the counter
+                //i = 1 - i;
+                t = t + 0.001;
+            }
+/*  	if( _CP0_GET_COUNT() >= (SYS_FREQ/1000/2)*1 )// count up to 1ms // greater than (SYS_FREQ /2*countingseconds)
+            {
+                //setVoltage('A', 0x00);
+                _CP0_SET_COUNT(0); // reset the counter
+                i = 1 - i;
+            }
+    if(i == 1)
+        setVoltage('A', 0x00);
+    else
+        setVoltage('A', 0x80);
+*/    
         
   
   }
